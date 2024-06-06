b4b88416080a5f316e5bea76e1f276a571f3e180
yooshf <158088664+yooshf@users.noreply.github.com>
2024-06-06 14:34:11 +0300

    Add files via upload

---
+++ android_project/One piece/app/src/main/java/com/example/recyclevapp/CustomeAdapter.java
@@ -0,0 +1,52 @@
+package com.example.recyclevapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class CustomeAdapter extends RecyclerView.Adapter<CustomeAdapter.MyViewHolder> {
+
+    private ArrayList<DataModel> dataSet;
+
+    public CustomeAdapter(ArrayList<DataModel> dataSet) {
+        this.dataSet = dataSet;
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewName;
+        TextView textViewDescription;
+        ImageView imageView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewName = itemView.findViewById(R.id.textView);
+            textViewDescription = itemView.findViewById(R.id.textView2);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+
+    @NonNull
+    @Override
+    public CustomeAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardrow, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomeAdapter.MyViewHolder holder, int position) {
+        holder.textViewName.setText(dataSet.get(position).getName());
+        holder.textViewDescription.setText(dataSet.get(position).getDescription());
+        holder.imageView.setImageResource(dataSet.get(position).getImageResource());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSet.size();
+    }
+}
